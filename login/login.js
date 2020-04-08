8c1e7af58801c04cfc448674e8720d1d8dc48bf5
harika34 <senapathiharika3434@gmail.com>
2020-04-08 18:58:54 +0530

    Login page

--- login/login.js
+++ login/login.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var validate = require("validate.js");
 var path = require('path');
 var mysql = require('mysql');
 
 var app = express();
+const port = process.env.port || 3000;
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'userdetails'
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'userdetails'
 });
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function (request, response) {
@@ -24,23 +26,19 @@ app.post('/auth', function (request, response) {
   var username = request.body.username;
   var password = request.body.password;
 
-  if (username && password) {
-    connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
-      if (results.length > 0) {
-        response.send('logged successfully');
-        
-      } else {
-        response.send('Incorrect Username and/or Password!');
-        
-      }
-      response.end();
-    });
-  } else {
-    response.send('Please enter Username and Password!');
+  connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+    if (results.length > 0) {
+      response.send('logged successfully');
+
+    } else {
+      response.send('Incorrect Username and/or Password!');
+
+    }
     response.end();
-  }
+  });
+
 });
 
 
-app.listen(3001);
+app.listen(port);
 
